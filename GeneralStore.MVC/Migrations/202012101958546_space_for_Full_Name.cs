50ae84df359b5cb10a7bc62abef29426c2e0bcea
carltgoss <ctgoss1840@gmail.com>
2020-12-10 17:14:48 -0500

    Customer added

---
+++ GeneralStore.MVC/Migrations/202012101958546_space_for_Full_Name.cs
@@ -0,0 +1,16 @@
+namespace GeneralStore.MVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class space_for_Full_Name : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
